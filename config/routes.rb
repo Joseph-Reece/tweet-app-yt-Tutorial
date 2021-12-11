d95b781b92a05f460cdc696b3330c49605359c1c
Joseph-Reece <ndirashjose@gmail.com>
2021-12-11 10:20:12 +0300

    tut 9: Urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # define the root path
+  root to: 'main#index'
+
+  # get 'main/index'
+  get '/about', to: 'about#index', as: 'about'
+  # get 'about/show'
 end
